6521af71fcc42614342873932b39b89b4e0320f0
borelle <krolleyami@yahoo.fr>
2015-12-29 15:23:06 +0100

    Merge origin/master

--- src/java/controller/AccueilAdmController.java
+++ src/java/controller/AccueilAdmController.java
@@ -5,14 +5,29 @@
  */
 package controller;
 
+
 import entities.Compte;
-//import entities.ClientConnecte;
+import entities.Typecompte;
+import java.security.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.swing.JOptionPane;
 import model.dao.AdministrateurDAO;
+import org.apache.taglibs.standard.functions.Functions;
+import static org.apache.tomcat.jni.OS.random;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -22,7 +37,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class AccueilAdmController {
     
     private final AdministrateurDAO auth = new AdministrateurDAO();
-    private final Compte adm = new Compte();
+    private Compte adm ;
     
     @RequestMapping(value = "user", method = RequestMethod.GET)
     protected String userAction(HttpServletRequest request, Model model) {
@@ -41,7 +56,7 @@ public class AccueilAdmController {
     protected String retourAccueilAction(Model model) {
         return "redirect:accueilAdm";
     }
-    @RequestMapping(value = "creer")
+    @RequestMapping(value = "Ajoutuser")
     protected String creerUserAction(Model model) {
         return "creerUtilisateur";
     }
@@ -63,5 +78,63 @@ public class AccueilAdmController {
         return "accueilAdm";
     }
     
+    //traitement de la page creation du client   
     
+    @RequestMapping(value = "CreationUser", method = RequestMethod.POST)
+    protected String boutonCreationUserAction(HttpServletRequest request, Model model) {
+        
+        String nom = request.getParameter("Nom");
+        String prenom = request.getParameter("Prenom");
+        String login = request.getParameter("Login");
+        String mdp = request.getParameter("Motdepasse");
+        int typec = Integer.parseInt(request.getParameter("Compte"));
+        boolean ajout = true;
+        
+        
+        try {
+            if(nom!=null && prenom!=null && login!=null && mdp!=null && typec!=0){
+               Typecompte tcpt = new Typecompte();
+               tcpt.setIdTypeCompte(typec);
+              /* byte[] bSalt = new byte[8];
+               random.nextBytes(bSalt);
+               String sSalt = byteToBase64(bSalt);*/
+               int nb = (int)Math.random()*10000+255;
+               String salt = Integer.toBinaryString(nb);
+               salt= auth.encode(salt);
+               salt= Functions.substring(salt, 0, 31);
+               mdp = auth.encode(mdp.concat(salt));
+               SimpleDateFormat formater = new SimpleDateFormat("dd-MM-yy hh:mm:ss");
+               Date date = new Date();
+               System.out.println(formater.format(date));
+               
+               adm = new Compte(tcpt, nom, prenom, login, mdp, salt, date );
+               //adm1 = new Compte(tcpt, nom, prenom, login, mdp, salt, null);
+                System.out.println("nom:"+ nom);
+                System.out.println("prenom:"+prenom);
+                System.out.println("login:"+login);
+                System.out.println("mdp:"+mdp);
+                System.out.println("typec:"+typec);
+                System.out.println("salt:"+salt);
+                System.out.println("date:"+date);
+                ajout = auth.addCompte(adm);
+                System.out.println("ajout:"+ajout); 
+
+            }else{
+                /*String st = "Tous les champs ne sont pas renseignés";
+                    JOptionPane.showMessageDialog(null,st);*/
+                System.out.println("veuillez renseigner tous les champs");
+            }
+            return "test";   
+        }catch (Exception e) {
+            System.out.println("catch du controller");   
+            e.printStackTrace();   
+           
+        }
+        return "compteUtilisateur";
+    }
+    
+    @RequestMapping(value = "AnnuleCreationUser", method = RequestMethod.POST)
+    protected String annuleCreationUserAction(Model model) {
+        return "compteUtilisateur";
+    }
 }
--- src/java/model/dao/AdministrateurDAO.java
+++ src/java/model/dao/AdministrateurDAO.java
@@ -6,9 +6,11 @@
 package model.dao;
 
 import entities.Compte;
+import entities.Typecompte;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.time.Clock;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -49,9 +51,6 @@ public class AdministrateurDAO {
 
         try {
             this.session = HibernateUtil.getSessionFactory().openSession();
-
-            /*Query query = session.createQuery("select cpt.nom, cpt.prenom, cpt.login, tcpt.libelle "
-             + "from compte as cpt inner join typecompte as tcpt on cpt.IdTypeCompte=tcpt.IdTypeCompte");*/
             Query query = session.createQuery("from Compte");
 
             list = (ArrayList<Compte>) query.list();
@@ -65,10 +64,26 @@ public class AdministrateurDAO {
         session.close();
         return null;
     }
-
-    public String encode(String sel) {
-
-        return sha256(" ".concat(sel));
+    /*public List<Typecompte> typeCompteUser(){
+        ArrayList<Typecompte> list = new ArrayList<Typecompte>();
+        
+        try{
+            this.session = HibernateUtil.getSessionFactory().openSession();
+            Query query = session.createQuery("from typecompte");
+            list = (ArrayList<Typecompte>) query.list();
+            
+            if (list.size() > 0) {
+                return list;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return list;
+    }*/
+   
+ 
+    public String encode(String mdp) {
+        return sha256(mdp);
     }
 
     public static String sha256(String base) {
@@ -90,4 +105,61 @@ public class AdministrateurDAO {
             throw new RuntimeException(ex);
         }
     }
+    
+    public boolean addCompte(Compte compte){
+    
+        /*try{
+            this.session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            String hql = "INSERT INTO compte(nom, prenom, login, password, idTypeCompte) "  + 
+             "SELECT nom, prenom, login, password, idTypeCompte FROM compte where nom=?, prenom=?, login=?, password=?, idTypeCompte=?";
+            Query query = session.createQuery(hql);
+            
+            query.setString(1, compte.getNom());
+            query.setString(2, compte.getPrenom());
+            query.setString(3, compte.getLogin());
+            query.setString(4, compte.getPassword());
+            query.setInteger(5, compte.getTypecompte().getIdTypeCompte());
+            int result = query.executeUpdate();
+            session.getTransaction().commit();
+            return true;
+        }catch(Exception e){
+            System.out.println("echec de connexion");
+            e.printStackTrace();
+        }
+        session.close();
+        HibernateUtil.getSessionFactory().close();
+        return false;*/
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            System.out.println("test 1");
+            //compte.setSalt("sel");
+            
+            session.beginTransaction();
+           
+            System.out.println("nom:"+compte.getNom());
+            System.out.println("prenom:"+compte.getPrenom());
+            System.out.println("login:"+compte.getLogin());
+            System.out.println("mdp:"+compte.getPassword());
+            System.out.println("date:"+compte.getCreation());
+            System.out.println("sel:"+compte.getSalt());
+            System.out.println("compte:"+compte.getTypecompte().getIdTypeCompte());
+            //session.persist(compte);
+            //session.update(compte);
+            session.save(compte);
+            System.out.println("test 3");
+            session.getTransaction().commit();
+            System.out.println("dans le try du dao");
+            return true;
+        } catch (Exception e) {
+            System.out.println("catch du dao"); 
+            e.printStackTrace();
+        }
+        session.close();
+        HibernateUtil.getSessionFactory().close();
+        
+        return false;
+    
+    }
 }
+
